083cb32fea3728904790d1410d312e80907091b0
vusatchy <vusatch.y@gmail.com>
2018-03-26 22:49:01 +0300

    adding scheduling

--- text_query_handlers/HandlersRegistration.py
+++ text_query_handlers/HandlersRegistration.py
@@ -4,12 +4,14 @@ from text_query_handlers import GreetingHandler as gh
 from text_query_handlers import HelloHandler as hh
 from text_query_handlers import RandomHandler as randh
 from text_query_handlers import LoveHandler as lh
+from  text_query_handlers import ScheduleHandler as sh
 
 
 class HandlersRegistration(object):
     handlers = []
 
     def registration(self):
+        self.handlers.append(sh.ScheduleHandler())
         self.handlers.append(ph.PingHandler())
         self.handlers.append(rh.RightHandler())
         self.handlers.append(gh.GreetingHandler())
---
+++ text_query_handlers/ScheduleHandler.py
@@ -0,0 +1,69 @@
+from text_query_handlers import AbstractHandler as ah
+from datetime import datetime, date, time
+import ConstantsAndUtils as cau
+
+
+class ScheduleHandler(ah.AbstractHandler):
+    am_right = {"скарлет", "дай", "розклад"}
+
+    shedule = {}
+    shedule["понеділок"] = [
+        "11:50-13:10 Філософія(лек) (266)",
+        "13:30-14:50 Оптика(лек) (365)",
+        "15:05-16:25 Оптика(прак) (261)",
+        "16:40-18:00 ЧМ(прак) (261,265)"
+    ]
+    shedule["вівторок"] = [
+        "15:05-16:25 ЧМ(лек ч) (111)",
+        "16:40-18:00 ЧМ(лек) (111)",
+        "18:10-19:30 Філософія(прак з) (150)"
+    ]
+    shedule["середу"] = [
+        "13:30-14:50 MO(прак з) (117)",
+        "15:05-16:25 БД(лек) (266)",
+        "16:40-18:00 БД(прак) (365)"
+    ]
+    shedule["четвер"] = [
+        "15:05-16:25 MCC(прак ч) (367)",
+        "15:05-16:25 MO(лек з) (111)",
+        "16:40-18:00 MCC(лек) (265)"
+    ]
+    shedule["п'ятницю"] = [
+        "11:50-13:10 QA(пр) (261)",
+        "13:30-14:50 QA(лаб) (261)"
+    ]
+
+    def predicate(self, message):
+        return cau.in_set(message.text, self.am_right)
+
+    def handle(self, bot, message):
+        keys = list(self.shedule.keys())
+        pointer = datetime.now().weekday()
+        if (cau.in_set(message.text, {"сьогодні"})):
+            if (pointer in range(len(keys))):
+                key = keys[pointer]
+                self.send_message_by_day(bot, message, key)
+                return
+        elif (cau.in_set(message.text, {"завтра"})):
+            pointer = pointer + 1
+            if (pointer in range(len(keys))):
+                key = keys[pointer]
+                self.send_message_by_day(bot, message, key)
+                return
+        elif (cau.in_set(message.text, {"вчора"})):
+            pointer = pointer - 1
+            if (pointer in range(len(keys))):
+                key = keys[pointer]
+                self.send_message_by_day(bot, message, key)
+                return
+
+        for key in keys:
+            if (cau.in_set(message.text, {key})):
+                self.send_message_by_day(bot, message, key)
+                return
+
+    def send_message_by_day(self, bot, message, key):
+        mess = ""
+        for word in self.shedule[key]:
+            mess = mess + word + "\n"
+        bot.send_message(message.chat.id, mess)
