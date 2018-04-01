d2053ef857a7bb2b814596b080bd36ffe73fc698
vusatchy <vusatch.y@gmail.com>
2018-04-01 15:56:04 +0300

    periodica task with days test

--- text_query_handlers/PeriodicTaskHandler.py
+++ text_query_handlers/PeriodicTaskHandler.py
@@ -34,7 +34,7 @@ class PeriodicTaskHandler(ah.AbstractHandler):
         #job.minutes.do(task, chat_id, phrase, bot)
         job.day.at(time).do(task, chat_id, phrase, bot)
         self.tasks[self.task_counter] = job
-        bot.send_message(chat_id, "Задача збережена успішно")
+        bot.send_message(chat_id, "Задача збережена успішно " + datetime.fromtimestamp(message.date).time().__str__())
 
     def task(self, chat_id, message, bot):
         bot.send_message(chat_id, message)
