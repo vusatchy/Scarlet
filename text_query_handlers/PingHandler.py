29dd82e8ace1b6e21ee45d93fa19aeb06950cc36
Yura_Vusach <yura.vusach@rci.rogers.com>
2020-10-04 15:44:19 +0300

    Updated greeting

--- text_query_handlers/PingHandler.py
+++ text_query_handlers/PingHandler.py
@@ -3,7 +3,7 @@ from text_query_handlers import AbstractHandler as ah
 
 class PingHandler(ah.AbstractHandler):
     def predicate(self, message):
-        return message.text == "Пінг"
+        return message.text == "Привіт"
 
     def handle(self, bot, message):
-        bot.send_message(message.chat.id, "Понг")
+        bot.send_message(message.chat.id, "Привіт")
